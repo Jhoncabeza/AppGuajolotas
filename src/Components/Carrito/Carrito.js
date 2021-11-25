0314f94ce526f0e3d1cb690351741ee2dcf8f157
Jhon Cabeza <cabejhon7@gmail.com>
2021-11-24 22:35:16 -0500

    FINALIZADO

--- src/Components/Carrito/Carrito.js
+++ src/Components/Carrito/Carrito.js
@@ -1,15 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ButtonCarrito, DivBotonCarrito, DivContainerFlechaCarrito, DivFlechitaAtras, DivH4PintarCarrito, DivH6P, DivImagenCarrito, DivImagenNoHayProductos, DivPintarDataCarrito, DivTituloCarrito, DivTodo, DivTotalCarrito, H1PrecioTotalCarrito, H1TotalCarrito, H4PintarCarrito, H5NoHayProductos, H6TituloPintarData, ImagenNoHayProductos, ImgAtras, ImgPintarDataCarrito, LinkAtras } from '../Home/HomeStyles'
 
 let noHayProductos = {
     "nombre": "No hay productos",
     "imagen": "https://res.cloudinary.com/dxgmhyliz/image/upload/v1637708981/Sprint_2/logo/Group_exs6pv.png"
 }
+let url = "https://datasprint2.herokuapp.com/carrito"
 
 const Carrito = () => {
-    const [productos, setProductos] = useState(noHayProductos)
-    
 
+    const [datas, setDatas] = useState(noHayProductos)
+
+    const getSearch = async (url) => {
+        let response = await fetch(url);
+        let data = await response.json();
+        setDatas(data)
+        
+    }
+     
+    useEffect(() => {
+        getSearch(url)
+    }, [])
+
+   
+    
+    
     return (
 
         <div>
@@ -24,7 +39,7 @@ const Carrito = () => {
 
 
             {
-                productos === noHayProductos ?
+                datas === noHayProductos ?
                     <DivImagenCarrito>
                         <DivImagenNoHayProductos>
                             <ImagenNoHayProductos src={noHayProductos.imagen} alt="" />
@@ -35,19 +50,34 @@ const Carrito = () => {
 
                     <DivPintarDataCarrito>
                         <DivTodo>
-                            <ImgPintarDataCarrito src="https://res.cloudinary.com/dxgmhyliz/image/upload/v1636775454/Sprint_2/Guajolotas/guajolota-rajas_tyegpo.png" alt="" />
+                            <ImgPintarDataCarrito src={datas[0].dataElegido.image} alt="" />
                             <DivH6P>
-                                <H6TituloPintarData>Guajolota de Tamal Verde</H6TituloPintarData>
-                                <H4PintarCarrito>x2</H4PintarCarrito>
+                                <H6TituloPintarData>{datas[0].dataElegido.name}</H6TituloPintarData>
+                                <H4PintarCarrito>x{datas[0].contador}</H4PintarCarrito>
                             </DivH6P>
                             <DivH4PintarCarrito>
-                                <H4PintarCarrito>$50 MXN</H4PintarCarrito>
+                                <H4PintarCarrito>${datas[0].dataElegido.price * datas[0].contador} MXN</H4PintarCarrito>
                             </DivH4PintarCarrito>
                         </DivTodo>
-                        
+
+                        {
+                            (datas[0].checkbox).map((el) => (
+                                <DivTodo key={el.id}>
+                                    <ImgPintarDataCarrito src={el.image} alt="" />
+                                    <DivH6P>
+                                        <H6TituloPintarData>{el.name}</H6TituloPintarData>
+                                        <H4PintarCarrito>x1</H4PintarCarrito>
+                                    </DivH6P>
+                                    <DivH4PintarCarrito>
+                                        <H4PintarCarrito>${el.price} MXN</H4PintarCarrito>
+                                    </DivH4PintarCarrito>
+                                </DivTodo>
+                            ))
+                        }
+
                         <DivTotalCarrito>
                             <H1TotalCarrito>Total</H1TotalCarrito>
-                            <H1PrecioTotalCarrito>$50 MXN</H1PrecioTotalCarrito>
+                            <H1PrecioTotalCarrito>${datas[0].total} MXN</H1PrecioTotalCarrito>
                         </DivTotalCarrito>
                     </DivPintarDataCarrito>
 
--- src/Components/Home/Modal.js
+++ src/Components/Home/Modal.js
@@ -5,8 +5,9 @@ import { Link } from 'react-router-dom'
 import {
     ButtonCarrito, ButtonMasContador, ButtonMenosContador, CheckBoxP, DivBotonCarrito, DivBotones,
     DivCajita, DivCarrito, DivCheckbox, DivContador, DivGuajolocombo, DivImgH4P, DivPintar, DivSabores,
-    H1NameSlider,H2NameSabor,H4ImgP,IconoMas, IconoMenos, ImageCheckBox, ImageDivPintar, ImgSabores, 
-    LinkCards, Pesos } from './HomeStyles'
+    H1NameSlider, H2NameSabor, H4ImgP, IconoMas, IconoMenos, ImageCheckBox, ImageDivPintar, ImgSabores,
+    LinkCards, Pesos
+} from './HomeStyles'
 import LogoCarrito from './LogoCarrito'
 
 
@@ -17,37 +18,73 @@ const Modal = () => {
     const [datos, setDatos] = useState({});
     const [contador, setContador] = useState(1);
     const [opacidadMenos, setOpacidadMenos] = useState("");
+
     const [checked, setChecked] = useState(false);
     const [checkboxPrecio, setCheckboxPrecio] = useState(0);
     const [checkBoxCantidad, setCheckBoxCantidad] = useState(0);
-    
+    const [shoppingCart, setShoppingCart] = useState([]);
+    let contadorCarrito = contador;
+    let dataElementoElegido = datos;
 
     useEffect(() => {
         setDatos(JSON.parse(localStorage.getItem('data')));
-    }, [])
+    },[])
 
     const cambioColor = (contador) => {
-        contador < 2 ? setOpacidadMenos("") :  setOpacidadMenos("active")
+        contador < 2 ? setOpacidadMenos("") : setOpacidadMenos("active")
     }
 
-    const handleChange = (e, precio) => {
+    
+    const addChange = (precio,ind) => {
+        setCheckboxPrecio(checkboxPrecio + precio);
+        setCheckBoxCantidad(checkBoxCantidad + 1);
+        let newCart = [...shoppingCart]
+        newCart.push(ind)
+        setShoppingCart(newCart)        
+    }
+    
+    const deleteChange = (precio,ind) => {
+        setCheckboxPrecio(checkboxPrecio - precio);
+        setCheckBoxCantidad(checkBoxCantidad - 1);
+        let newCart = [...shoppingCart]
+        let indexFound = newCart.findIndex((element) => element.id === ind.id)
+        newCart = newCart.filter((element, index) => index !== indexFound)
+        setShoppingCart(newCart)   
+    }
+
+    const handleChange = (e, precio, ind) => {
         let bandera = e.target.checked
         setChecked(e.target.checked);
-        if (bandera === true) {
-            setCheckboxPrecio(checkboxPrecio + precio)
-            setCheckBoxCantidad(checkBoxCantidad + 1)
-        } else if (bandera === false) {
-            setCheckboxPrecio(checkboxPrecio - precio)
-            setCheckBoxCantidad(checkBoxCantidad - 1)
+        if (bandera !== undefined) {
+            bandera ?
+            addChange(precio,ind)
+            :
+            deleteChange(precio,ind)
         }
     };
 
+    const handlePost = async () => {
+        await fetch('https://datasprint2.herokuapp.com/carrito', {
+            method: 'POST',
+            body: JSON.stringify({
+                "contador":contadorCarrito,
+                "dataElegido":dataElementoElegido,
+                "checkbox":shoppingCart,
+                "total":(datos.price * contador) + checkboxPrecio
+
+            }),
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8"
+            }
+        })
+        window.location.reload(true);
+    }
 
     return (
         <div>
             <DivCarrito>
                 <LinkCards to='/Home'><FontAwesomeIcon icon={faArrowLeft} /></LinkCards>
-                <Link style={{textDecoration:"none",color:"gray"}} to='/Car'><LogoCarrito /></Link>
+                <Link style={{ textDecoration: "none", color: "gray" }} to='/Car'><LogoCarrito /></Link>
             </DivCarrito>
 
             <DivPintar>
@@ -71,7 +108,7 @@ const Modal = () => {
                 {
 
                     dataSabores.map((ind) => (
-                        <ImgSabores onClick={() => {setDatos(ind)}}key={ind.id} src={ind.flavor.imageFlavor} alt="" />
+                        <ImgSabores onClick={() => { setDatos(ind) }} key={ind.id} src={ind.flavor.imageFlavor} alt="" />
                     ))
 
                 }
@@ -80,11 +117,11 @@ const Modal = () => {
             <p>Selecciona la combinacion que mas te guste y disfruta de tu desayuno</p>
             <DivGuajolocombo>
                 {
-                   
+
                     dataGuajolocombo.map((ind) => (
                         <DivCheckbox key={ind.id}  >
                             <DivCajita>
-                                <input type="checkbox" onClick={(e) => { (handleChange(e, ind.price)) }} />
+                                <input type="checkbox" onChange={(e) => { handleChange(e, ind.price, ind) }} />
                             </DivCajita>
                             <DivImgH4P>
                                 <ImageCheckBox src={ind.image} alt="" />
@@ -93,12 +130,12 @@ const Modal = () => {
                             </DivImgH4P>
                         </DivCheckbox>
                     ))
-                     
+
                 }
-                
+
             </DivGuajolocombo>
             <DivBotonCarrito>
-                <ButtonCarrito onClick ={(e)=>{console.log(e)}}>Agregar {contador + checkBoxCantidad} al carrito  ${(datos.price * contador) + checkboxPrecio} MXN</ButtonCarrito>
+                <ButtonCarrito onClick={() => { handlePost() }}>Agregar {contador + checkBoxCantidad} al carrito  ${(datos.price * contador) + checkboxPrecio} MXN</ButtonCarrito>
             </DivBotonCarrito>
 
 
